980c2fb33c9a53693d6868c83c44ed075d186eb2
alvarolopezrenovell <alvaro@tourknife.com>
2020-12-03 16:11:31 +0100

    Add feed list

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Feed;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,7 +18,8 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->render('home/index.html.twig');
+        $feeds = $this->getDoctrine()->getRepository(Feed::class)->findAll();
+        return $this->render('home/index.html.twig', ['feeds' => $feeds]);
     }
 
 }
