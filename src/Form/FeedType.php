564b3d5da3e6264563d9872d232605b73c4908d8
alvarolopezrenovell <alvaro@tourknife.com>
2020-12-03 14:34:40 +0100

    Add form to create and edit feed

---
+++ src/Form/FeedType.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace App\Form;
+
+
+use App\Entity\Feed;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FeedType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+          ->add('title', TextType::class)
+          ->add('body', TextType::class)
+          ->add('image', TextType::class)
+          ->add('source', TextType::class)
+          ->add('publisher', TextType::class)
+          ->add('submit', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        return [
+          'data_class' => Feed::class
+        ];
+    }
+
+
+}
