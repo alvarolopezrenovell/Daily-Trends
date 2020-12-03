49b98c5c2df61729f304e393d26e64431ff1195f
alvarolopezrenovell <alvaro@tourknife.com>
2020-12-03 20:06:50 +0100

    Add FeedFactory

---
+++ src/Factory/FeedFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+
+namespace App\Factory;
+
+
+use App\Entity\Feed;
+
+class FeedFactory
+{
+
+    /**
+     * @param $title
+     * @param $body
+     * @param $image
+     * @param $source
+     * @param $publisher
+     * @param $publishedAt
+     * @return Feed
+     */
+    public static function create(string $title, string $body, string $image, string $source, string $publisher, \DateTime $publishedAt) {
+        $feed = new Feed();
+
+        $feed->setTitle($title);
+        $feed->setBody($body);
+        $feed->setImage($image);
+        $feed->setSource($source);
+        $feed->setPublisher($publisher);
+        $feed->setPublishedAt($publishedAt);
+
+        return $feed;
+    }
+
+}
